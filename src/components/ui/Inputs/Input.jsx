6433d4fe895d2b29a1da7b3195208b9b5199f39c
inasai <82827520+inasai@users.noreply.github.com>
2023-06-16 17:36:53 +0300

    cart

--- src/components/ui/Inputs/Input.jsx
+++ src/components/ui/Inputs/Input.jsx
@@ -1,14 +1,20 @@
 import React, { useRef } from 'react';
 import cn from 'classnames';
 
+<<<<<<< HEAD
 const Input = ({ name, value, onChange, type, placeholder, className }) => {
   const inputRef = useRef(null);
+=======
+const Input = ({ name, value, onChange, type = 'text', placeholder, className }) => {
+	const inputRef = useRef(null);
+>>>>>>> 0a91851f5956336a2df7d173d9e1672f46d0b0ed
 
   const handleChange = (e) => {
     const value = e.target.value;
     onChange(value);
   };
 
+<<<<<<< HEAD
   return (
     <div className={cn('input__wrapper', className)}>
       <input
@@ -22,6 +28,21 @@ const Input = ({ name, value, onChange, type, placeholder, className }) => {
       />
     </div>
   );
+=======
+	return (
+		<div className={cn("input__wrapper", className)}>
+			<input
+				ref={inputRef}
+				className={cn("input", className)}
+				placeholder={placeholder}
+				name={name}
+				type={type}
+				value={value}
+				onChange={handleChange}
+			/>
+		</div>
+	);
+>>>>>>> 0a91851f5956336a2df7d173d9e1672f46d0b0ed
 };
 
 export default Input;
